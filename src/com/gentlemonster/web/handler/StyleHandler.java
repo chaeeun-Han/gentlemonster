2e23e7e58d19cdff50bc4f0eb4dd1c0b8e5f0962
lsj90954511 <sojin49@naver.com>
2024-12-31 16:54:32 +0900

    feat: Add style handler

---
+++ src/com/gentlemonster/web/handler/StyleHandler.java
@@ -0,0 +1,26 @@
+package com.gentlemonster.web.handler;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.gentlemonster.web.dao.StyleDAO;
+import com.gentlemonster.web.dto.StyleDTO;
+
+public class StyleHandler implements CommandHandler{
+	
+	private StyleDAO styleDAO;
+	
+	public StyleHandler() {
+		styleDAO = new StyleDAO();
+	}
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) {
+		List<StyleDTO> styles = styleDAO.getStyleList();
+		request.setAttribute("styles", styles);
+		return "/styles/list.jsp";
+	}
+
+}
