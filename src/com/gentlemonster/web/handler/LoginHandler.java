622424bab5d4207fdf7bc2a90a57b5dffe013b0c
lsj90954511 <sojin49@naver.com>
2025-01-02 21:09:22 +0900

    feat: Add Login and Join

--- src/com/gentlemonster/web/handler/LoginHandler.java
+++ src/com/gentlemonster/web/handler/LoginHandler.java
@@ -7,33 +7,41 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.gentlemonster.web.RequestMapping;
-import com.gentlemonster.web.dto.CustomerDTO;
+import com.gentlemonster.web.dao.MemberDAO;
 
-@RequestMapping("/login")
+@RequestMapping("/member/login")
 public class LoginHandler implements CommandHandler{
 
-	CustomerDTO customer;
+	private MemberDAO memberDAO;
+	
 	public LoginHandler() {
-		customer = new CustomerDTO();
+		memberDAO = new MemberDAO();
 	}
 	
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) {
 		
-		// 로그인 기능 구현 시 바뀌어야 할 부분
-		customer.setCustomerId(1L);
-		customer.setUserId("id");
-		customer.setUserPw("password");
-		customer.setBirthday(new Timestamp(System.currentTimeMillis()));
-		customer.setEmail("metanet@naver.com");
-		customer.setAddress("서울");
-		
-		
-		HttpSession session = request.getSession();
-		session.setAttribute("customer", customer);
-		
-		return "/shop/sunglassesList.jsp";
+		if ("POST".equalsIgnoreCase(request.getMethod())) {
+			String userid = request.getParameter("userid");
+			String password = request.getParameter("password");
+			HttpSession session = request.getSession();
 			
+			try {
+				String dbpw = memberDAO.getPassword(userid);
+
+				if (dbpw.equals(password)) {
+					session.setAttribute("userid", userid);
+				} else {
+					throw new RuntimeException("비밀번호가 다릅니다.");
+				}
+			} catch(RuntimeException e) {
+				session.invalidate();
+				e.printStackTrace();
+			}
+			return "redirect:/shop?bigCategoryId=1&smallCategoryId=view-all";
+		} else {
+			return "/shop/sunglassesList.jsp";
+		}	
 	}
 	
 }
